221aa589069364d87cd97a404bec1689dbae0b11
katya <katya.gurina@yahoo.com>
2017-03-28 20:18:32 -0400

    sidebar done, front page featured projects slider started

---
+++ public/script/script.js
@@ -0,0 +1,22 @@
+
+
+ $(document).ready(adjustPageWidth);
+
+ $(window).resize(adjustPageWidth);
+
+
+// this function adjusts page width to accomodate for the sidebar
+
+ function adjustPageWidth(){
+  var pageWidth = $(document).width();
+  if(pageWidth < 780){
+  	$(".page-body").css("margin-left", "0px");
+  	console.log(pageWidth);
+  	console.log("margin removed")
+  }
+  else {
+  	$(".page-body").css("margin-left", "300px");
+  	console.log(pageWidth);
+  	console.log("margin added");
+  }
+ }

1a4813a86fe56212476f1bb2127e3919bd655021
katya <katya.gurina@yahoo.com>
2017-03-29 00:04:28 -0400

    deployed on heroku

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    cool = require('cool-ascii-faces');
 
+
+ app.set('views', __dirname + '/views');
  app.set("view engine", "ejs");
+
+ app.set('port', (process.env.PORT || 5000));
  app.use(express.static(__dirname + "/public"));
+
 
 
  app.get("/", function(req, res){
  	res.render("home");
  });
 
+ app.get('/cool', function(req, res){
+ 	res.send(cool());
+ });
 
 
 
- app.listen(3000, function(){
- 	console.log("Server started");
- })
+
+ app.listen(app.get('port'), function(){
+ 	console.log( 'Node app is running on port', app.get('port'));
+ });

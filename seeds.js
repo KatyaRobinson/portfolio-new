8bd0bf4761c424cef00a417d1325bfca0e5bd7ef
katya <katya.gurina@yahoo.com>
2017-04-12 23:43:37 -0400

    back-to button for individual projects page

--- seeds.js
+++ seeds.js
@@ -3,6 +3,9 @@ var Project = require("./models/project");
 var Comment = require("./models/comment");
 var data = [
 			{
+
+				// featured projects
+				// Movement Source Website
 				id: 1,
 				featured: true,
 				name: "Movement Source",
@@ -12,6 +15,7 @@ var data = [
 				tools: "Bootstrap, jQuery, PHP, MySQL"
 			},
 			{
+				//Wikiepedia Viever
 				id: 2,
 				featured: true,
 				name: "Wikipedia Viewer",
@@ -21,6 +25,7 @@ var data = [
 				tools: "Bootstrap, jQuery, Wikipedia API"
 			},
 			{
+				// Random Quote Generator
 				id: 3,
 				featured: true,
 				name: "Random Quote Generator",
@@ -28,10 +33,78 @@ var data = [
 				shortDescription: "A web application that pulls random quote from http://quotesondesign.com API",
 				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
 				tools: "Bootstrap, jQuery, QuotesOnDesign API"
+			},
+
+			//pure css images
+
+			{
+				// Temporary page
+				id: 4,
+				featured: false,
+				name: "Temporary Page",
+				image: "/images/purecss/temp-page.png",
+				shortDescription: "A Pure CSS webpage I've created to show at my personal website URL as I was working on the new version of my portfolio",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
+			},
+
+			{
+				// Adopt don't shop
+				id: 5,
+				featured: false,
+				name: "Adopt, Don't Shop",
+				image: "/images/purecss/adopt.png",
+				shortDescription: "A Pure CSS page I created for the #DaylyCSS challenge",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
+			},
+
+			{
+
+				// Beaver
+				id: 6,
+				featured: false,
+				name: "Beaver",
+				image: "/images/purecss/beaver.png",
+				shortDescription: "A Pure CSS page I created for the #DaylyCSS challenge",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
+			},
+
+			{
+				// Elephant
+				id: 7,
+				featured: false,
+				name: "Elephant",
+				image: "/images/purecss/elephant.png",
+				shortDescription: "A Pure CSS page I created for the #DaylyCSS challenge",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
+			},
+
+			{
+				// Bear
+				id: 8,
+				featured: false,
+				name: "Bear",
+				image: "/images/purecss/bear.png",
+				shortDescription: "A Pure CSS page I created for the #DaylyCSS challenge",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
+			},
+
+			{
+				// Clock
+				id: 9,
+				featured: false,
+				name: "Clock",
+				image: "/images/purecss/clock.png",
+				shortDescription: "A Pure CSS page I created for the #DaylyCSS challenge",
+				description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur",
+				tools: "Pure CSS"
 			}
 
 			];
-
 
 function seedDB(){
 
@@ -60,3 +133,8 @@ function seedDB(){
 };
 
 module.exports = seedDB;
+
+
+
+
+
